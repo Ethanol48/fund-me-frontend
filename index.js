71d4a60f8ddb7a9574f499af30eb40f4a691330e
ethan <contact@ethan-rouimi.com>
2023-07-14 15:05:31 +0100

    adding logs to website

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
-import { abi, contractAddress } from "./constants.js"
+import { abi, contractAddress, INITIAL_BLOCK } from "./constants.js"
 
 const balanceDisplay = document.getElementById("balanceText")
 const connectButton = document.getElementById("connectButton")
 const withdrawButton = document.getElementById("withdrawButton")
 const fundButton = document.getElementById("fundButton")
 const balanceButton = document.getElementById("balanceButton")
+const logButton = document.getElementById("getLogs")
+const loadingLogs = document.querySelector(".hidden")
+const logsDisplay = document.getElementById("logsDisplay")
+
+console.log(loadingLogs)
+
+logButton.onclick = getFundedLogs
 connectButton.onclick = connect
 withdrawButton.onclick = withdraw
 fundButton.onclick = fund
@@ -39,7 +46,8 @@ async function connect() {
     if (connected) {
       connectButton.style.display = "none"
       const accounts = await ethereum.request({ method: "eth_accounts" })
-      console.log(accounts)
+      getFundedLogs()
+      getBalance()
     }
   } else {
     connectButton.innerHTML = "Please install MetaMask"
@@ -115,3 +123,70 @@ function listenForTransactionMine(transactionResponse, provider) {
     })
   })
 }
+
+function getTemplateLogsAndAppendToDOM(
+  funderAddress,
+  amountEth,
+  parentElement,
+  txHash
+) {
+  const logContainer = document.createElement("div")
+
+  logContainer.classList.add("log-container")
+
+  parentElement.appendChild(logContainer)
+
+  const funderElem = document.createElement("div")
+  funderElem.classList.add("funder")
+  const fundLink = document.createElement("a")
+
+  funderElem.innerHTML = `funder: `
+
+  // take half of the length of the funder address and add 3 points to it '...'
+  fundLink.innerHTML = `${funderAddress}`.slice(0, 20) + "..."
+  fundLink.href = `https://sepolia.etherscan.io/tx/${txHash}`
+
+  funderElem.appendChild(fundLink)
+
+  const amountElem = document.createElement("p")
+  amountElem.classList.add("amount")
+  amountElem.innerHTML = `amount: ${amountEth} ETH`
+
+  logContainer.appendChild(funderElem)
+  logContainer.appendChild(amountElem)
+
+  return logContainer
+}
+
+async function getFundedLogs() {
+  console.log(`Getting funded logs...`)
+
+  logsDisplay.innerHTML = ""
+
+  loadingLogs.classList.toggle("hidden")
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const contract = new ethers.Contract(contractAddress, abi, provider)
+  const filter = contract.filters.Funded()
+  const logs = await contract.queryFilter(
+    filter,
+    INITIAL_BLOCK,
+    provider.blockNumber
+  )
+
+  console.log(logs)
+
+  console.log(`funded logs`)
+  logs.forEach((log) => {
+    const funder = log.args[0]
+    const funds = ethers.utils.formatEther(log.args[1].toString())
+    const tranxHash = log.transactionHash
+
+    getTemplateLogsAndAppendToDOM(funder, funds, logsDisplay, tranxHash)
+  })
+
+  logButton.innerHTML = "Refresh Logs"
+  loadingLogs.classList.toggle("hidden")
+}
+
+// getFundedLogs()
